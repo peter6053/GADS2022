23337238d6498e9fd0f6537081ef328113f86f79
peter6053 <peternjuguna76@gmail.com>
2022-05-26 15:46:07 +0300

    inital build

---
+++ app/src/main/java/com/example/itemmenu/datamanager.kt
@@ -0,0 +1,22 @@
+package com.example.itemmenu
+class datamanager {
+    val corses = hashMapOf<String, Courseinfo>()
+    val notes = ArrayList<NoteInfo>()
+
+
+    fun initializecourses() {
+        var course = Courseinfo("android_intents","Android progrsming with intents")
+        corses.set(course.courseId,course)
+
+        course = Courseinfo(courseId = "Android async","Andrid asyn programing and services")
+        corses.set(course.title,course)
+
+
+        course = Courseinfo(title = "Java funndamental the java language", courseId = "java_lang")
+        corses.set(course.courseId,course)
+
+        course = Courseinfo(courseId = "Java_core", title = "java fundamental the core platform")
+        corses.set(course.courseId,course)
+    }
+
+}
---
+++ app/src/main/java/com/example/itemmenu/notekeeperdata.kt
@@ -0,0 +1,4 @@
+package com.example.itemmenu
+
+class Courseinfo (val courseId:String, val title:String)
+class NoteInfo (var course: Courseinfo, var tittle: String, val text: String)
